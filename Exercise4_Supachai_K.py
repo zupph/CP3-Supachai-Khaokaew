03ee7874b00f72c525189852e68a5fa2d6c539af
zupph <mrzupph@gmail.com>
2020-01-26 19:26:52 +0700

    Create Exercise4_Supachai_K.py

---
+++ Exercise4_Supachai_K.py
@@ -0,0 +1,9 @@
+FoundEnglish = 6.1
+GeneralBusiness = 5.5
+IntroCompSystems = 5.3
+CompProgramming = 5.4
+print("--- Your Score ---")
+print("Foundation English : ", FoundEnglish)
+print("General Business : ", GeneralBusiness)
+print("Introduction to Computer Systems : ", IntroCompSystems)
+print("Computer Programming : ", CompProgramming)
