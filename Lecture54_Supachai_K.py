6ca6108f0518395f84ddc1788ccdacb1784ed3d0
zupph <mrzupph@gmail.com>
2020-06-06 23:03:26 +0700

    sector 8 lacture 54

---
+++ Lecture54_Supachai_K.py
@@ -0,0 +1,35 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return True
+    else:
+        return False
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    return userSelected
+
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+
+if login() == True:
+    showMenu()
+    userSelected = menuSelect()
+    if userSelected == 1:
+        totalPrice = int(input("please enter total price : "))
+        print(vatCalculator(totalPrice))
+    elif userSelected == 2:
+        print(priceCalculator())
