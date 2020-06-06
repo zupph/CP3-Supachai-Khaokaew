1c5ce331a31f17c6fc07999d219611b8b3a1dcf7
zupph <mrzupph@gmail.com>
2020-06-06 20:10:27 +0700

    section 8 lecture50

---
+++ Lecture50_Supachai_K.py
@@ -0,0 +1,12 @@
+def addNumber(x, y):
+    print(x + y)
+def minusNumber(x, y):
+    print(x - y)
+def multiplyNumber(x, y):
+    print(x * y)
+def diviveNumber(x, y):
+    print(x / y)
+addNumber(10, 2)
+minusNumber(10, 2)
+multiplyNumber(10, 2)
+diviveNumber(10, 2)
