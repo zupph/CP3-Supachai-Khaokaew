ce698c6b2aff8ca03e16e1afc598d10e8584289f
zupph <mrzupph@gmail.com>
2020-06-23 18:59:35 +0700

    show file list in listbox fc19

--- renman.py
+++ renman.py
@@ -1,11 +1,18 @@
 from tkinter import *
 from tkinter import filedialog
+import os
 
 
 def file_dialog(event):
     file_dialog = filedialog.askdirectory()
     file_text.delete(0, END)
     file_text.insert(0, file_dialog)
+    selected_dir = file_text.get()
+    os.chdir(selected_dir)
+    file_list = os.listdir()
+    listbox.delete(0, END)
+    for i in file_list:
+        listbox.insert(END, i)
 
 
 main_window = Tk()
@@ -22,10 +29,6 @@ listbox = Listbox(frame, width=50, height=20)
 listbox.pack(side=LEFT, fill=Y)
 scrollbar = Scrollbar(frame, orient="vertical")
 scrollbar.pack(side=RIGHT, fill=Y)
-
-for i in range(100):
-    listbox.insert(END, i)
-
 listbox.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=listbox.yview)
 
